a81a6a9cbbb81d933eb710c86cb5b407b9b83ff2
Ryan040709 <ryanma040709@gmail.com>
2026-01-17 10:59:29 -0500

    [WIP] Turret Code w/ limelight setup

---
+++ src/main/java/frc/robot/subsystems/TurretTest.java
@@ -0,0 +1,148 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.lang.annotation.Target;
+
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+//import frc.robot.SwerveConstants.TunerSwerveDrivetrain;
+
+
+
+/** Add your docs here. */
+public class TurretTest extends SubsystemBase {
+    //our motor
+    private TalonFX m_turret = new TalonFX(10);
+    //said motor's pos, request stuff
+    private PositionVoltage m_request = new PositionVoltage(0);
+
+    private final double goldenAngle = 33.73877/90;
+
+    private final double NinetyDegreeRotation = 33.73877;
+
+    //
+    //private final double ticksPerAngleRatio = NinetyDegreeRotation*(360/90);
+
+    private final double ticksPerAngle = NinetyDegreeRotation/90;
+
+    public static int kPigeonId = 14;
+
+    private final Pigeon2 m_gyro = new Pigeon2(6, "rio"); // Pigeon is on roboRIO CAN Bus with device ID 1
+    
+
+    //limelight goofy ahh stuff
+    // Basic targeting data
+    double tx = LimelightHelpers.getTX("limelight-turret");  // Horizontal offset from crosshair to target in degrees
+    double ty = LimelightHelpers.getTY("limelight-turret");  // Vertical offset from crosshair to target in degrees
+    double ta = LimelightHelpers.getTA("limelight-turret");  // Target area (0% to 100% of image)
+    boolean hasTarget = LimelightHelpers.getTV("limelight-turret"); // Do you have a valid target?
+
+    double txnc = LimelightHelpers.getTXNC("limelight-turret");  // Horizontal offset from principal pixel/point to target in degrees
+    double tync = LimelightHelpers.getTYNC("limelight-turret");  // Vertical  offset from principal pixel/point to target in degrees
+
+    public TurretTest(){
+        TalonFXConfiguration motorConfig = new TalonFXConfiguration();
+        motorConfig.MotorOutput.PeakForwardDutyCycle = 0.75;
+        motorConfig.MotorOutput.PeakReverseDutyCycle = -0.75;
+        motorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        motorConfig.Slot0.kP = 1; 
+        motorConfig.Slot0.kI = 0.15; 
+        motorConfig.Slot0.kD = 0;
+        motorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+        motorConfig.CurrentLimits.StatorCurrentLimit = 100;
+        motorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        motorConfig.CurrentLimits.SupplyCurrentLimit = 100;
+        motorConfig.CurrentLimits.SupplyCurrentLowerLimit = 40;
+        motorConfig.CurrentLimits.SupplyCurrentLowerTime = 1;
+        motorConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+        motorConfig.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 35;
+        motorConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+        motorConfig.SoftwareLimitSwitch.ReverseSoftLimitThreshold = -35;
+
+        //Voltage
+        motorConfig.Voltage.PeakForwardVoltage = 16;
+        motorConfig.Voltage.PeakReverseVoltage = -16;
+        //Differential Constants
+        motorConfig.DifferentialConstants.PeakDifferentialDutyCycle = 1;
+        motorConfig.DifferentialConstants.PeakDifferentialTorqueCurrent = 800;
+        motorConfig.DifferentialConstants.PeakDifferentialVoltage = 16;
+        //Motion Magic
+        motorConfig.MotionMagic.MotionMagicCruiseVelocity = 100;
+        motorConfig.MotionMagic.MotionMagicAcceleration = 150;
+        motorConfig.MotionMagic.MotionMagicExpo_kA = 0.10000000149011612;
+        motorConfig.MotionMagic.MotionMagicExpo_kV = 0.11999999731779099;
+        //Torque Current
+        motorConfig.TorqueCurrent.PeakForwardTorqueCurrent = 800;
+        motorConfig.TorqueCurrent.PeakReverseTorqueCurrent = -800;
+
+        m_turret.getConfigurator().apply(motorConfig);
+
+    }
+    
+    public void MoveMotor(double targetSpeed){
+        if(m_turret.getPosition().getValueAsDouble()>-goldenAngle || m_turret.getPosition().getValueAsDouble()<goldenAngle){
+            m_turret.set(targetSpeed);
+        }
+        else{
+            m_turret.set(0);
+        }
+     
+    }
+    //SmartDashboard.putNumber("Bridge Angle", bridgeTipper.getPosition());
+    
+    @Override
+    public void periodic() {
+    //StatusSignal<Angle> robotYaw = m_gyro.getYaw();  
+    
+    Angle robotYaw = m_gyro.getYaw().getValue();  
+
+LimelightHelpers.SetRobotOrientation("limelight-turret", robotYaw, 0.0, 0.0, 0.0, 0.0, 0.0);
+
+// Get the pose estimate
+LimelightHelpers.PoseEstimate limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue("");
+
+// Add it to your pose estimator
+m_poseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(.5, .5, 9999999));
+m_poseEstimator.addVisionMeasurement(
+    limelightMeasurement.pose,
+    limelightMeasurement.timestampSeconds
+);
+
+
+
+        SmartDashboard.putNumber("Turret Angle", m_turret.getPosition().getValueAsDouble());
+
+        SmartDashboard.putNumber("tx", LimelightHelpers.getTX("limelight-turret"));
+        SmartDashboard.putNumber("ty", LimelightHelpers.getTY("limelight-turret"));
+    }
+
+    //run to golden angle
+    //basic stuff, could probably be made better
+
+
+    public void zeroPosition(){
+        m_turret.setPosition(0);
+    }
+    public void setPosition( double angle){ 
+        //private final double position = angle*(ticksPerAngle);
+        m_turret.setControl(m_request.withPosition(angle*(ticksPerAngle)));
+    }
+}
