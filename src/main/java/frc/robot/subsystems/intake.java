e177c501c8af37505cea48c4fee18ed7c659f456
Caedmon Weiler <Caedmon.weiler@gmail.com>
2026-01-27 19:31:50 -0500

    Update intake.java
    `

--- src/main/java/frc/robot/subsystems/intake.java
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -2,7 +2,10 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
-public class intake {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class intake extends SubsystemBase{
 
     private TalonFX intakeMotor1 = new TalonFX(10);
     private TalonFX intakeMotor2 = new TalonFX(10);
@@ -13,6 +16,33 @@ public class intake {
 
     public void MoveintakeMotor2(double targetSpeed) {
         intakeMotor2.set(targetSpeed);
+    }
+
+    public Command IntakeToHopperCommand() {
+        return runOnce(() -> {
+            MoveintakeMotor2(-0.5);
+            MoveintakeMotor1(.5);
+        });
+    }
+
+    public Command IntakeToTurretCommand() {
+        return runOnce(() -> {
+            MoveintakeMotor2(-0.5);
+            MoveintakeMotor1(.5);    
+        });
+    }
+    public Command HopperToTurretCommand() {
+        return runOnce(() -> {
+            MoveintakeMotor2(-0.5);
+            MoveintakeMotor1(.5);
+        });
+    }
+
+    public Command HopperToIntakeCommand() {
+        return runOnce(() -> {
+            MoveintakeMotor2(-0.5);
+            MoveintakeMotor1(.5);
+        });
     }
 
 }
