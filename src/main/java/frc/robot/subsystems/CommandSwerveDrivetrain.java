9916515a78b45e8f739374955a9a87c55c322d65
Caedmon Weiler <Caedmon.weiler@gmail.com>
2026-02-16 20:23:06 -0500

    cleaned the comments in commandswervedrivetrain

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -89,7 +89,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
             m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation,
             m_backRightLocation);
-    // setting up odomitree
+    // setting up odometery
     private SwerveDrivePoseEstimator poseEstimator;
     private Field2d m_Fields = new Field2d();
     public HolonomicDriveController controller;
