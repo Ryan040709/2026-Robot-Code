a186072f9a131ff6b0d5a6dcb0017e661b0fc63c
Brian Salva <king.doge.the.thurd@gmail.com>
2026-02-20 19:20:10 -0500

    added turret position math
    the current code assume that the turret is on the center of the robot. The new positioning math uses trigonometry to estimate where its at on the robot.

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,8 +1,12 @@
 package frc.robot.subsystems;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
+
+import org.opencv.core.Mat;
+
 import com.ctre.phoenix6.configs.TalonFXSConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.Pigeon2;
@@ -40,6 +44,11 @@ public class TurretSubsystem extends SubsystemBase {
     Translation2d blueHubPos = new Translation2d(4.62554, 4.03606); // blue hub
 
     Translation2d robotPos = new Translation2d(0, 0); // robot position
+
+    // offset is converted from inches to meters
+
+    // gear ratio
+    // 18:168
 
     public double redFx = 11.98482 + (4.62554 / 2); // blueFeedingX
     public double blueFx = 4.62554 / 2; // blueFeedingX
@@ -80,7 +89,6 @@ public class TurretSubsystem extends SubsystemBase {
 
     public double robotVelocityX;
     public double robotVelocityY;
-
 
     public double turretError = turretTARGET - turret.getPosition().getValueAsDouble();
 
@@ -234,6 +242,17 @@ public class TurretSubsystem extends SubsystemBase {
         return poseSupplier.get();
     }
 
+    public Translation2d turretPosition() { // until we have the actual robot, this function is to be UNUSED!
+        // radius of the turret-robotCenter
+        double r = Math.sqrt(Math.pow(Constants.TurretSubsystem.Turret_TurretX, 2)
+                + Math.pow(Constants.TurretSubsystem.Turret_TurretY, 2));
+        // translated values
+        double x2 = r * Math.cos(-theta + 90); // formula to translate the turret, where x2 acts as the translated "x"
+        double y2 = r * Math.sin(-theta + 90); // formula to translate the turret, where y2 acts as the translated "y"
+
+        return new Translation2d(x2, y2);
+    }
+
     public void zeroPosition() {
         turret.setPosition(0);
     }
@@ -339,7 +358,9 @@ public class TurretSubsystem extends SubsystemBase {
                 }
                 isFeeding = true;
             } else {
-                lockingTarget = new Translation2d(Hx + (velocityX*ShooterSubsystem.tof), redHubPos.getY() + (velocityY*ShooterSubsystem.tof)); // TODO test this before getting the real robot!
+                lockingTarget = new Translation2d(Hx + (velocityX * ShooterSubsystem.tof),
+                        redHubPos.getY() + (velocityY * ShooterSubsystem.tof)); // TODO test this before getting the
+                                                                                // real robot!
                 isFeeding = false;
             }
         } else {
@@ -351,7 +372,8 @@ public class TurretSubsystem extends SubsystemBase {
                 }
                 isFeeding = true;
             } else {
-                lockingTarget = new Translation2d(Hx + (velocityX*ShooterSubsystem.tof), redHubPos.getY() + (velocityY*ShooterSubsystem.tof)); // so does this system actually work?
+                lockingTarget = new Translation2d(Hx + (velocityX * ShooterSubsystem.tof),
+                        redHubPos.getY() + (velocityY * ShooterSubsystem.tof)); // so does this system actually work?
                 isFeeding = false;
             }
         }
