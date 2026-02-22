b6dd6cbe53b88d6ef7419938e685625a630e404b
Caedmon Weiler <Caedmon.weiler@gmail.com>
2026-02-21 21:22:50 -0500

    cleaned

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -23,7 +23,7 @@ public class ClimberSubsystem extends SubsystemBase {
     public final TalonFX climberMotorB = new TalonFX(12);
     public final CANcoder climberCANcoder = new CANcoder(13);
 
-    Servo climbServo = new Servo(1); // I think this one is currently being used by the LED strip.
+    Servo climbServo = new Servo(1);
 
     private MotionMagicVoltage request;
 
