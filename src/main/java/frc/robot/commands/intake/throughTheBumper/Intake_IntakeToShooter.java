e6bf684afa8a1c2aa514300989618aa0117446a2
Ryan040709 <ryanma040709@gmail.com>
2026-03-02 19:37:51 -0500

    made conceptual SWIM stuff

--- src/main/java/frc/robot/commands/intake/throughTheBumper/Intake_IntakeToShooter.java
+++ src/main/java/frc/robot/commands/intake/throughTheBumper/Intake_IntakeToShooter.java
@@ -35,7 +35,7 @@ public class Intake_IntakeToShooter extends Command {
 
         outtaBumperIntakeSubsystem.PivotIntake(0);
         if(outtaBumperIntakeSubsystem.AtPosition(0)){
-        intakeSubsystem.SetIntakeFront(-.4);
+        intakeSubsystem.SetIntakeFront(-.75);
         intakeSubsystem.SetIntakeBack(-.75);
         }
         
