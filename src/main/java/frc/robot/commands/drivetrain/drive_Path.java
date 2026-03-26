d1dc68a6291a394f46aa459d0339f5cb4de864f1
Lucas Schulz <LucasJohnschulz@gmail.com>
2026-03-26 19:10:17 -0400

    more split path auto code
    added an if statement to drive_Path, does not do anything right now

--- src/main/java/frc/robot/commands/drivetrain/drive_Path.java
+++ src/main/java/frc/robot/commands/drivetrain/drive_Path.java
@@ -1,5 +1,8 @@
 package frc.robot.commands.drivetrain;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
@@ -14,24 +17,33 @@ public class drive_Path extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    if(true){
+      try{
+        delegate = AutoBuilder.followPath(PathPlannerPath.fromPathFile("splitPath2"));
+      }catch(Exception e){}
+    }else{
+      try{
+        delegate = AutoBuilder.followPath(PathPlannerPath.fromPathFile("splitPath3"));
+      }catch(Exception e){}
+    }
+    delegate.initialize();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    delegate.execute();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-
+    delegate.end(interrupted);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return delegate.isFinished();
   }
 }
